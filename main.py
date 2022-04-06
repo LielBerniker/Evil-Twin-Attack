5934ef9a60941dd9bacfa9b547a406690c5e1188
liel berniker <lielb101995@gmail.com>
2022-04-06 12:52:17 +0300

    add function that show all the current wifi available

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import os
+# We require regular expressions.
+import re
+# We will be using the subprocess module to run commands on Kali Linux.
+import subprocess
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def find_all_wifi_available():
+    # Regex to find wireless interfaces. We're making the assumption they will all be wlan0 or higher.
+    wlan_pattern = re.compile("^wlan[0-9]+")
 
+    # Python allows is to run system commands by using a function provided by the subprocess module.
+    # subprocess.run(<list of command line arguments goes here>)
+    # The script is the parent process and creates a child process which runs the system command,
+    # and will only continue once the child process has completed.
+    # We run the iwconfig command to look for wireless interfaces.
+    check_wifi_result = wlan_pattern.findall(subprocess.run(["iwconfig"], capture_output=True).stdout.decode())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # No WiFi Adapter connected.
+    if len(check_wifi_result) == 0:
+        print("Please connect a WiFi adapter and try again.")
+        exit()
+
+    # Menu to select WiFi interface from
+    print("The following WiFi interfaces are available:")
+    for index, item in enumerate(check_wifi_result):
+        print(f"{index} - {item}")
+    # Ensure the WiFi interface selected is valid. Simple menu with interfaces to select from.
+    while True:
+        wifi_interface_choice = input("Please select the interface you want to use for the attack: ")
+        try:
+            if check_wifi_result[int(wifi_interface_choice)]:
+                break
+        except:
+            print("Please enter a number that corresponds with the choices available.")
+
+    # For easy reference we call the selected interface hacknic
+    hacknic = check_wifi_result[int(wifi_interface_choice)]
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    find_all_wifi_available()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
