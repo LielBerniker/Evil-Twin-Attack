f4f887a49631dcc72e026cb74186d5a1d919da78
liel berniker <lielb101995@gmail.com>
2022-05-10 18:26:15 +0000

    add create ap

--- create_ap.py
+++ create_ap.py
@@ -14,37 +14,27 @@ import time
 from threading import Thread 
 from string import Template
 
-WifiAdapter = ""
-def change_template(path):
+def change_template(path,interface,ssid_name):
     with open(path, 'r+') as f:
         template = Template(f.read())
         f.seek(0)
-        f.write(template.substitute(INTERFACE=WifiAdapter))
+        f.write(template.substitute(INTERFACE=interface,SSID_NAME=ssid_name))
         f.truncate()
 
-def prepare_fake_access_point():
+def prepare_fake_access_point(interface,ssid_name):
     """
     prepare the environment setup for creating the fake access point
     :param access_point_bssid represent the network name
     """
-    os.system('rm -rf build/')
+    os.system('rm -rf build_up/')
     os.system('cp -r configuration_files build_up')
-    change_template('build_up/hostapd.conf')
-    change_template('build_up/dnsmasq.conf')
-    change_template('build_up/prepare_ap.sh')
-    change_template('build_up/reboot.sh')
+    change_template('build_up/hostapd.conf',interfacessid_name)
+    change_template('build_up/dnsmasq.conf',interface,ssid_name)
+    change_template('build_up/prepare_ap.sh',interface,ssid_name)
+    change_template('build_up/reboot.sh',interface,ssid_name)
     print("finish prepare conf files")
     os.system('sudo sh build_up/prepare_ap.sh')
 
-if __name__ == "__main__":
-
-    #finding wifi adapter
-    # WifiAdapter = WifiAdapterFinder()
-    #changing adapter to monitor mode
-    # MonitorMode(WifiAdapter)
-    #scanning for wifi network to attack
-    # prepare_fake_access_point()
-    # time.sleep(120)
     os.system('sudo sh configuration_files/reboot.sh')
 
 
