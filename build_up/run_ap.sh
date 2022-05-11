40451195745807f3bff6f31a87dd3bb08cbb73b8
YairRaviv <yairaviv83@gmail.com>
2022-05-11 13:18:56 +0300

    AYAA

--- build_up/run_ap.sh
+++
@@ -1,6 +0,0 @@
-
-dnsmasq -C build_up/dnsmasq.conf
-gnome-terminal -- sh -c "npm --prefix ./captive_portal/ start"
-route add default gw 10.0.0.1
-hostapd build_up/hostapd.conf -B
-route add default gw 10.0.0.1
